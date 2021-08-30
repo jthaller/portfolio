9b8a5f2a09afd91c52ad7d4aa4c28628e5ea38c5
Jeremy Thaller <jeremy@thallerfamily.com>
2021-08-30 13:37:01 -0400

    added pdf api peview

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,62 @@
         target: "#sideNav",
     });
 })(jQuery); // End of use strict
+
+
+// Start of adobe embed api section
+/*
+Copyright 2020 Adobe
+All Rights Reserved.
+
+NOTICE: Adobe permits you to use, modify, and distribute this file in
+accordance with the terms of the Adobe license agreement accompanying
+it. If you have received this file from a source other than Adobe,
+then your use, modification, or distribution of it requires the prior
+written permission of Adobe.
+*/
+
+/* Pass the embed mode option here */
+const viewerConfig = {
+    embedMode: "LIGHT_BOX"
+};
+
+/* Wait for Adobe Document Services PDF Embed API to be ready and enable the View PDF button */
+document.addEventListener("adobe_dc_view_sdk.ready", function () {
+    document.getElementById("view-pdf-btn").disabled = false;
+});
+
+/* Function to render the file using PDF Embed API. */
+function previewFile()
+{
+    /* Initialize the AdobeDC View object */
+    var adobeDCView = new AdobeDC.View({
+        /* Pass your registered client id */
+        clientId: "<3c54d68ce6b84ee2a6b0474a66317f7d>"
+    });
+
+    /* Invoke the file preview API on Adobe DC View object */
+    adobeDCView.previewFile({
+        /* Pass information on how to access the file */
+        content: {
+            /* Location of file where it is hosted */
+            location: {
+                url: "https://raw.githubusercontent.com/jthaller/short_resume/fd9a6f99c33061fbd4b50cb3b3e7e79f1537545a/Jeremy_Thaller_Resume.pdf",
+                /*
+                If the file URL requires some additional headers, then it can be passed as follows:-
+                header: [
+                    {
+                        key: "<HEADER_KEY>",
+                        value: "<HEADER_VALUE>",
+                    }
+                ]
+                */
+            },
+        },
+        /* Pass meta data of file */
+        metaData: {
+            /* file name */
+            fileName: "Jeremy_Thaller_Resume.pdf"
+        }
+    }, viewerConfig);
+};
+
