9248ff41e94f55c4e063120829fabea9596885f8
Jeremy Thaller <jeremythaller@Jeremys-MacBook-Pro-14.local>
2024-01-24 13:03:15 -0500

    matching main to gh-pages code-wise

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,10 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
     */
-    (function ($) {
+
+const GITHUB_URL = "https://raw.githubusercontent.com/jthaller/resume_url_hosting/32236b74a33e69f6e12ffd6354e38c95ddcc5ed4/Jeremy_Thaller_Resume.pdf"
+   
+   (function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
@@ -40,3 +43,64 @@
         target: "#sideNav",
     });
 })(jQuery); // End of use strict
+
+
+// Start of adobe embed api section
+/*
+Copyright 2020 Adobe
+All Rights Reserved.
+
+NOTICE: Adobe permits you to use, modify, and distribute this file in
+accordance with the terms of the Adobe license agreement accompanying
+it. If you have received this file from a source other than Adobe,
+then your use, modification, or distribution of it requires the prior
+written permission of Adobe.
+*/
+
+/* Pass the embed mode option here */
+const viewerConfig = {
+    embedMode: "LIGHT_BOX",
+    defaultViewMode: "FIT_PAGE"
+};
+
+/* Wait for Adobe Document Services PDF Embed API to be ready and enable the View PDF button */
+document.addEventListener("adobe_dc_view_sdk.ready", function () {
+    document.getElementById("view-pdf-btn").disabled = false;
+});
+
+/* Function to render the file using PDF Embed API. */
+function previewFile()
+{
+    /* Initialize the AdobeDC View object */
+    var adobeDCView = new AdobeDC.View({
+        /* Pass your registered client id */
+        clientId: "3c54d68ce6b84ee2a6b0474a66317f7d"
+    });
+
+    /* Invoke the file preview API on Adobe DC View object */
+    adobeDCView.previewFile({
+        /* Pass information on how to access the file */
+        content: {
+            /* Location of file where it is hosted */
+            location: {
+                url: "GITHUB_URL",
+
+                // url: "https://github.com/jthaller/short_resume/raw/master/Jeremy_Thaller_Resume.pdf"
+                /*
+                If the file URL requires some additional headers, then it can be passed as follows:-
+                header: [
+                    {
+                        key: "<HEADER_KEY>",
+                        value: "<HEADER_VALUE>",
+                    }
+                ]
+                */
+            },
+        },
+        /* Pass meta data of file */
+        metaData: {
+            /* file name */
+            fileName: "Jeremy_Thaller_Resume.pdf"
+        }
+    }, viewerConfig);
+};
